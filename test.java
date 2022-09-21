ec4e4162344903d9ad06d72ff826062d25fbd904
Vedant Chawla <vedantchawla06@gmail.com>
2022-09-21 14:57:31 +0530

    added message in Sum.java

---
+++ test.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class test{
+    public void main(String[] args)throws IOException{
+        int a = 4;
+        int b = 5;
+
+        System.out.println("The sum is "+ a + b);
+    }
+}
